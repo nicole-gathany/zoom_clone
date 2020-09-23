155b3591d50ee0e6c721b8822bc24539777e32ee
Nicole Gathany <nicole.gathany@gmail.com>
2020-09-22 22:52:58 -0400

    added routes to server and views of the rooms for styling the rooms

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+//allows a server to be used with socket.io
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidv4 } = require('uuid')
+
+//Setting up express servers
+//ejs is the library we added earlier when downloading dependencies and how the views will be rendered
+app.set('view engine, ejs')
+//javascript and css will go into the public folder
+app.use(express.static('public'))
+
+
+//for the get route
+//req is request
+//res is response
+app.get('/', (req, res) => {
+    //will give us a random uuid
+    //random rooms
+    res.redirect(`/${uuidv4()}`)
+})
+
+//routes for rooms
+app.get('/:room',  (req, res) => {
+    res.render('room', {roomId: req.params.room})
+})
+
+server.listen(3000)
