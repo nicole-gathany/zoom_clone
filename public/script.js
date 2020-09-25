81a29b12fa089d658addae94746d0bf4e7753b28
Nicole Gathany <nicole.gathany@gmail.com>
2020-09-25 00:01:51 -0400

    users officially have their own ids but for some reason the server keeps crashing

--- public/script.js
+++ public/script.js
@@ -5,8 +5,11 @@ const myPeer = new Peer(undefined, {
 
 })
 
+myPeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id)
+})
 
-socket.emit('join-room', ROOM_ID, 10)
+
 
 
 //rooms are still not connecting for some reason. I will need to go back and look tomorrow. 
