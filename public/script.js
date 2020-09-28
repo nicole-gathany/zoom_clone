d718bca06f91ad7cf9716bc8f1845514cc96c412
Nicole Gathany <nicole.gathany@gmail.com>
2020-09-27 22:41:10 -0400

    fixed an issue with the peer library

--- public/script.js
+++ public/script.js
@@ -1,20 +1,17 @@
 const socket = io('/')
-// const myPeer = new Peer(undefined, {
-//     host: '/', 
-//     port: '3001'
-
-// })
-
-//is making the app crash, but i also am not connected to the room, 
-//i think i may have to kill the peer server and restart it
-// myPeer.on('open', id => {
-//     socket.emit('join-room', ROOM_ID, id)
-// })
 
 
+//creates a random user id
+const myPeer = new Peer(undefined, {
+    host: '/', 
+    port: '3001'
+})
+
+myPeer.on('open', id =>{
+    socket.emit('join-room', ROOM_ID, id)
+})
 
 
-//rooms are still not connecting for some reason. I will need to go back and look tomorrow. 
 socket.on('user-connected', userId => {
-    console.log('User connected: ' + userId )
+    console.log('User connected:' + userId)
 })
--- server.js
+++ server.js
@@ -1,36 +1,31 @@
-const express = require('express');
-const app = express();
-//allows a server to be used with socket.io
+const express = require('express')
+const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { v4: uuidv4 } = require('uuid')
+const { v4: uuidV4 } = require('uuid')
 
-//Setting up express servers
-//ejs is the library we added earlier when downloading dependencies and how the views will be rendered
 app.set('view engine', 'ejs')
-//javascript and css will go into the public folder
 app.use(express.static('public'))
 
+//home page
 
-//for the get route
-//req is request
-//res is response
-app.get('/', (req, res) => {
-    //will give us a random uuid
-    //random rooms
-    res.redirect(`/${uuidv4()}`)
+app.get('/', (req, res) =>{
+    res.redirect(`/${uuidV4()}`)
 })
 
-//routes for rooms
-app.get('/:room',  (req, res) => {
-    res.render('room', {roomId: req.params.room})
+//room
+
+app.get('/:room', (req, res) =>{
+    res.render('room', { roomId: req.params.room })
 })
 
-io.on('connection', socket =>{
-    socket.on('join-room', (roomId, userId) => {
+//connects anytime someone connects to our webpage
+io.on('connection', socket => {
+    //anytime we join a room, we get a roomId and a userId
+    socket.on('join-room', (roomId, userId) =>{
         socket.join(roomId)
         socket.to(roomId).broadcast.emit('user-connected', userId)
-    })
+    }) 
 })
 
 server.listen(3000)
