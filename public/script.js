f0ba0343ea16fb90c755b531db9c05c5edb5057d
Nicole Gathany <nicole.gathany@gmail.com>
2020-09-27 23:08:09 -0400

    sound now works and user can see the person they're calling and themselves

--- public/script.js
+++ public/script.js
@@ -15,6 +15,16 @@ navigator.mediaDevices.getUserMedia({
     audio: true
 }).then(stream => {
     addVideoStream(myVideo, stream)
+
+    myPeer.on('call', call => {
+        call.answer(stream)
+    })
+
+    socket.on('user-connected', userId =>{
+        //when a new user connects
+        connectToNewUser(userId, stream)
+
+    })
 })
 
 myPeer.on('open', id =>{
@@ -22,14 +32,21 @@ myPeer.on('open', id =>{
 })
 
 
-socket.on('user-connected', userId => {
-    console.log('User connected:' + userId)
-})
-
 function addVideoStream(video, stream) {
     video.srcObject = stream;
     video.addEventListener('loadedmetadata', () =>{
         video.play()
     })
     videoGrid.append(video)
+}
+
+function connectToNewUser(userId, stream){
+    const call = myPeer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () =>{
+        video.remove()
+    })
 }
